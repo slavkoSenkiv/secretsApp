cc20a608bc625659c0b46f9d134fc18f52640187
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-05-30 21:49:55 +0300

    add mongose boilerplate and build user model

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const User = require('./User');
 
 const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+//mongoose boilerplate
+const url = "mongodb://127.0.0.1:27017/usersDB"; 
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('mongoose connection to local db is successful'))
+.catch((error) => console.error('Error connecting to local db:', error));
 
 app.get('/', (req, res)=>{
     res.render('home');
