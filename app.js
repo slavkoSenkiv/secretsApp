e346eeab4ed9164f4a2e22c9d717074f38d005a2
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-05-30 21:42:03 +0300

    add packs boilerplate code and get methods for home login and register pages

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-//jshint esversion:6
+const express = require('express');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+    res.render('home');
+});
+app.get('/login', (req, res)=>{
+    res.render('login');
+});
+app.get('/register', (req, res)=>{
+    res.render('register');
+});
+
+app.listen(3000, ()=>{
+    console.log('server is up and listedning to port 3000');
+});
