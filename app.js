7da8e9c16a5d4d240e1dac4688a7064f02ee83df
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-05-31 08:16:05 +0300

    add register and login post methods and thier functionality

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+//packs boilerplate
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const User = require('./User');
-
 const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
@@ -17,6 +17,7 @@ mongoose.connect(url, {
 .then(() => console.log('mongoose connection to local db is successful'))
 .catch((error) => console.error('Error connecting to local db:', error));
 
+//get methods
 app.get('/', (req, res)=>{
     res.render('home');
 });
@@ -25,6 +26,41 @@ app.get('/login', (req, res)=>{
 });
 app.get('/register', (req, res)=>{
     res.render('register');
+});
+
+app.post('/register', (req, res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save()
+    .then(()=>{
+        console.log('passed input');
+        res.render('secrets');
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+});
+
+app.post('/login', (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username})
+        .then((foundUser)=>{
+            if(foundUser.password === password){
+                res.render('secrets');
+            }else{
+                res.send('invalid password');
+                console.log('invalid password');
+            };
+        })
+        .catch((error)=>{
+            res.send('invalid login');
+            console.log(error);
+        });
 });
 
 app.listen(3000, ()=>{
