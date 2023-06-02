664376e0445e6f3dce1c8649883ea89be741473d
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-02 07:30:43 +0300

    add salted pass recognition to login input

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltingRounds = 10;
 
 //mongoose boilerplate
 const url = "mongodb://127.0.0.1:27017/usersDB"; 
@@ -30,32 +31,40 @@ app.get('/register', (req, res)=>{
 });
 
 app.post('/register', (req, res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    console.log('hashed pass from registration', md5(req.body.password));
+    bcrypt.hash(req.body.password, saltingRounds, (err, hash)=>{
+        
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
 
-
-    newUser.save()
-    .then(()=>{
-        console.log('passed input');
-        res.render('secrets');
+        newUser.save()
+        .then(()=>{
+            console.log('passed input');
+            res.render('secrets');
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
     })
-    .catch((error)=>{
-        console.log(error);
-    });
+    
 });
 
 app.post('/login', (req, res)=>{
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     console.log('hashed pass from login', password);
 
     User.findOne({email: username})
         .then((foundUser)=>{
-            if(foundUser.password === password){
-                res.render('secrets');
+            if(foundUser){
+                bcrypt.compare(password, foundUser.password, (err, result)=>{
+                    if(result==true){
+                        res.render('secrets');
+                    }else{
+                        console.log(err);
+                    }
+                });
             }else{
                 res.send('invalid password');
                 console.log('invalid password');
